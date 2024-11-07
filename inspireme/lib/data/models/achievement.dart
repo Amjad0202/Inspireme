200c2817664b519f9a17ee145d955db33e155619
Amjad0202 <mamamjadali@gmail.com>
2024-11-07 11:21:51 +0530

    Add Achievement model and JournalEditorScreen for managing journal entries

---
+++ inspireme/lib/data/models/achievement.dart
@@ -0,0 +1,22 @@
+// lib/data/models/achievement.dart
+class Achievement {
+  final String id;
+  final String title;
+  final String description;
+  final String icon;
+  final bool isUnlocked;
+  final DateTime? unlockedAt;
+  final int progressCurrent;
+  final int progressTotal;
+
+  Achievement({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.icon,
+    this.isUnlocked = false,
+    this.unlockedAt,
+    this.progressCurrent = 0,
+    required this.progressTotal,
+  });
+}
